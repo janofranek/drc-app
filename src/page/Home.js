1b99accb55ea3b46d1b9c775bc76dbd59369d3b2
janofranek <jan.franek@gmail.com>
2023-05-02 20:07:14 +0200

    remove mongo server

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { signOut } from "firebase/auth";
-import { auth } from '../firebase';
+import { auth } from '../cred/firebase';
 import Button from 'react-bootstrap/Button'
  
 const Home = (props) => {
--- src/page/notes.js
+++ src/page/notes.js
@@ -1,57 +1,4 @@
 /*
-            <main >        
-                <section>
-                    <div>                                            
-                        <p> DRCapp </p>                       
-                                                       
-                        <form>                                              
-                            <div>
-                                <label htmlFor="email-address">
-                                    Email address
-                                </label>
-                                <input
-                                    id="email-address"
-                                    name="email"
-                                    type="email"                                    
-                                    required                                                                                
-                                    placeholder="Email address"
-                                    onChange={(e)=>setEmail(e.target.value)}
-                                />
-                            </div>
-
-                            <div>
-                                <label htmlFor="password">
-                                    Password
-                                </label>
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"                                    
-                                    required                                                                                
-                                    placeholder="Password"
-                                    onChange={(e)=>setPassword(e.target.value)}
-                                />
-                            </div>
-                                                
-                            <div>
-                                <button                                    
-                                    onClick={onLogin}                                        
-                                >      
-                                    Login                                                                  
-                                </button>
-                            </div>                               
-                        </form>
-                       
-                        <p className="text-sm text-white text-center">
-                            No account yet? {' '}
-                            <NavLink to="/signup">
-                                Sign up
-                            </NavLink>
-                        </p>
-                                                   
-                    </div>
-                </section>
-            </main>
 
 
     <Router>
@@ -67,40 +14,6 @@
       </div>
     </Router>
 
-
-        <main className="form-signin">
-        <form>
-            
-            <h1 className="h3 mb-3 fw-normal">DRCapp - Přihlášení</h1>
-
-            <div className="form-floating">
-            <input 
-                type="email" 
-                className="form-control" 
-                id="email-address" 
-                name="email"
-                required
-                placeholder="name@example.com"
-                onChange={(e)=>setEmail(e.target.value)}/>
-            <label htmlFor="email-address">Email</label>
-            </div>
-            <div className="form-floating">
-            <input 
-                type="password" 
-                className="form-control" 
-                id="password"
-                name="password"
-                required                                                                                
-                placeholder="Heslo"
-                onChange={(e)=>setPassword(e.target.value)}
-            />
-            <label htmlFor="password">Heslo</label>
-            </div>
-
-            <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={onLogin}>Přihlásit</button>
-            
-        </form>
-        </main>
 
 
 IlavyneprNRrORioszfHTYD4S4zR14ZYyWo4ejHvliQxSonKUuPLy7kIjy5of14l
